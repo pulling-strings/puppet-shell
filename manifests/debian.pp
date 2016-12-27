652e4eb19bbba2e6263f6539c526f42f93d3739d
narkisr <narkisr@gmail.com>
2016-12-27 22:55:01 +1100

    z is part of oh-my-zsh now

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -3,7 +3,6 @@ class shell::debian {
   include shell::dots
   include shell::zsh
   include shell::oh_my_zsh
-  include shell::z
   include shell::ack
   include shell::rlwrap
   include shell::tmux
